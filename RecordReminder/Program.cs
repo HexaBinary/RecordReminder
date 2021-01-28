aee4dea8672dcf6969a4da2ef223177ac3cf62fa
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2021-01-28 11:06:25 +0300

    Update Program.cs

--- RecordReminder/Program.cs
+++ RecordReminder/Program.cs
@@ -7,22 +7,20 @@ namespace RecordReminder
     {
         static void Main(string[] args)
         {
+            int hour, minute, second;
+
             Console.WriteLine("Teams Toplantı Kayıt Hatırlatıcısına Hoş Geldiniz");
             Console.WriteLine("Eğer programlar ilgili soru veya probleminiz olur ise,");
             Console.WriteLine("https://github.com/HexaBinary/RecordReminder adresinde bir issue oluşturarak veya");
             Console.WriteLine("hexabinary2007@protonmail.com adresine eposta göndererek bana ulaşabilirsiniz");
+
             while (true)
             {
-                int hour = DateTime.Now.Hour;
-                int minute = DateTime.Now.Minute;
-                int second = DateTime.Now.Second;
+                hour = DateTime.Now.Hour;
+                minute = DateTime.Now.Minute;
+                second = DateTime.Now.Second;
 
-                if(hour == 9 && minute == 1 && second == 0)
-                {
-                    Console.WriteLine("Dersinizin başlamasından 1 dakika geçti, eğer daha başlatmadıysanız toplantı kaydı başlatmanız önerilir!");
-                    Thread.Sleep(1000);
-                }
-                else if(hour == 9 && minute == 36 && second == 0)
+                if ((hour == 9 && minute == 1 && second == 0) || (hour == 9 && minute == 36 && second == 0))
                 {
                     Console.WriteLine("Dersinizin başlamasından 1 dakika geçti, eğer daha başlatmadıysanız toplantı kaydı başlatmanız önerilir!");
                     Thread.Sleep(1000);
